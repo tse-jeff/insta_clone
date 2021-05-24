43385831abe6e8493be498b9ba17e1736f4af666
tse-jeff <jtse0316@gmail.com>
2021-05-24 18:58:27 -0400

    new folders for architecture

--- src/index.js
+++ src/index.js
@@ -3,3 +3,19 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+// client side rendered app: react (cra)
+    // -> database: Firebase
+    // -> react-loadking-skeleton
+    // tailwind
+
+// architecture
+    // -> components, 
+    // -> constants, 
+    // -> context, 
+    // -> helpers, 
+    // -> hooks, 
+    // -> pages, 
+    // -> lib (Firebase here)
+    // -> service (firebase functions)
+    // -> styles (tailwind)
