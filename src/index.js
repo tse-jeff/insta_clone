ca43676272dcb1247c5508a4ed1e69f7e2589805
tse-jeff <jtse0316@gmail.com>
2021-06-01 23:23:20 -0400

    firestore rules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import FirebaseContext from "./context/firebase"
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
